7a6c4a08ceecc768d1a325bfd1d52dd576891e2c
Tommaso <valverdetommaso@gmail.com>
2026-01-14 10:03:47 -0300

    att

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+let vel = 20
+console.log(`A velocidade do seu carro é ${vel} Km/h.`)
+if(vel>50){console.log('Voçê ultrapassou a velocidade permitida!. MULTADO!!!')}
+else{console.log(`Dirija sempre usando cinto de segunrança!`)}
+
