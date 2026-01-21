90ae4a3ee5ba22dfe74d745355522d630406d8cd
Tommaso <valverdetommaso@gmail.com>
2026-01-21 08:30:24 -0300

    att

---
+++ aula16/ambiente.js
@@ -0,0 +1,31 @@
+function parimp(n){
+    if(n % 2 == 0){
+        return 'par'
+    }else{
+        return 'impar'
+    }
+}
+
+console.log(parimp(223).toUpperCase())
+
+function soma(n1 = 0, n2 = 0){
+    return n1 + n2
+}
+
+console.log(soma(237))
+
+let v = function(x){
+    return x*2
+}
+
+console.log(v(5))
+
+function fatorial(n){
+    let fat = 1
+    for(let c = n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
