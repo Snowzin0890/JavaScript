d02890b667567ecdd1fc465eafa46ff9f6491426
Tommaso <valverdetommaso@gmail.com>
2026-01-21 08:36:17 -0300

    att

--- aula16/ambiente.js
+++ aula16/ambiente.js
@@ -29,3 +29,14 @@ function fatorial(n){
 }
 
 console.log(fatorial(5))
+
+
+function fato(n){
+    if(n == 1){
+        return 1
+    }else{
+        return n * fato(n - 1)
+    }
+}
+
+console.log(fato(5))
