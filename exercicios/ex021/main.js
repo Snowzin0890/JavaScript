917a91831c71070c15c98dbdec290646cc39af11
Tommaso <valverdetommaso@gmail.com>
2026-01-22 08:51:33 -0300

    att

--- exercicios/ex021/main.js
+++ exercicios/ex021/main.js
@@ -1,18 +1,26 @@
 let produtos = []
 let quantidade = []
-let preco = [calc()]
+let lista = {
+    banana:2,
+    maca:1,
+    pera:4,
+    manga:7,
+    abacaxi:10
+}
 
 function add(){
     let prod = document.querySelector('input#prod')
     let quant = document.querySelector('input#quant')
     let res = document.querySelector('div#res')
     let n = Number(quant.value)
-    let p = prod.value
+    let p = prod.value.toLowerCase()
 
     res.innerHTML = ''
 
-    if(prod.value.length == 0 || quant.value.length == 0 || produtos.indexOf(`${prod}`) != -1){
+    if(prod.value.length == 0 || quant.value.length == 0 || produtos.indexOf(p) != -1){
         alert('Insira produto e quantidade para adicionar! E nao repita um produto ja adicionado')
+    }else if(lista[p] === undefined){
+        alert('Produto nao encontrado na lista')
     }else{
         produtos.push(`${p}`)
         quantidade.push(n)
@@ -23,5 +31,24 @@ function add(){
 }
 
 function calc(){
-    
+    let res = document.querySelector('div#res')
+
+    if(produtos.length == 0 || quantidade.length ==0){
+        alert('Adicione itens a lista antes de calcular!')
+        return
+    }
+
+        res.innerHTML = '' 
+        let totalGeral = 0
+
+        for(let i = 0; i < produtos.length; i++){
+            let produto = produtos[i]
+            let quant = quantidade[i]
+            let precoUnitario = lista[produto]
+            let total = precoUnitario * quant
+            totalGeral += total
+
+            res.innerHTML += `<p>Voce adicionou ${quant} ${produto} que ficou R$ ${total}</p>`
+        }
+        res.innerHTML += `<p><strong>Total gasto: R$ ${totalGeral}<strong></p>`
 }
